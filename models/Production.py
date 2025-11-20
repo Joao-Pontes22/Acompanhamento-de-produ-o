9bd2cb172bb91da7fe73824901e8d31dad52f811
João Pontes <136093514+Joao-Pontes22@users.noreply.github.com>
2025-11-20 15:41:48 -0300

    Update Production.py

--- models/Production.py
+++ models/Production.py
@@ -5,10 +5,10 @@ from sqlalchemy.orm import relationship
 # Tabela de produção da usinagem
 # Machining production table
 
-class MachiningProduction (base):
+class Machining_Production (base):
     __tablename__ = "MachiningProduction"
 
-    serial_iD = Column("Serial_ID", Integer, primary_key=True, autoincrement=True)
+    serial_iD = Column("serial_ID", Integer, primary_key=True, autoincrement=True)
     date = Column("date", DateTime )
     part_number = Column("part_number", String)
     production_batch = Column("production_batch", String)
@@ -16,7 +16,8 @@ class MachiningProduction (base):
     id_employer = Column("id_employer", Integer, ForeignKey("employers.ID"))
     status = Column("status", String)
 
-    machiningemployer = relationship("Employers", back_populates="machiningproduction")
+    machining_employer = relationship("Employers", back_populates="machining_production")
+    assembly_production = relationship("Assembly_Production", back_populates="machining")
 
     def __init__(self, date, part_number,
                  production_batch, employer, id_employer, status):
@@ -27,14 +28,21 @@ class MachiningProduction (base):
         self.id_employer = id_employer
         self.status = status
 
-class AssemblyProduction (base):
+class Assembly_Production (base):
     __tablename__="AssemblyProduction"
 
     serial_ID = Column("ID", Integer, primary_key=True, autoincrement=True)
     part_number = Column("part_number", String)
-    components_ID = Column("components_ID", Integer, ForeignKey("MachiningProduction.serial_ID"))
+    components_ID = Column("components_ID", Integer, ForeignKey("Machining_Production.serial_ID"))
+    id_employer = Column("id_employer", Integer, ForeignKey("employers.ID"))
+    status = Column("status", String)
+    initial_hour = Column("initial_hour", DateTime)
+    final_hour = Column("final_hour", DateTime)
+    machine = Column("machine", String)
+    
+    assemblyemployer = relationship("Employers", back_populates="assembly_production")
+    machining = relationship("Machining_Production", back_populates="assembly_production")
 
-    assemblyemployer = relationship("Employers", back_populates="assemblyproduction")
 # -------------------------------------------------------------------#
 # Tabela de sucatas
 # Scraps table
