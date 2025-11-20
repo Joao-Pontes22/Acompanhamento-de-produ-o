189961e55acf5886764d095620279079237e2739
João Pontes <136093514+Joao-Pontes22@users.noreply.github.com>
2025-11-20 14:45:08 -0300

    Update Empolyers.py

--- models/Empolyers.py
+++ models/Empolyers.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, Column, String, Integer, Float, DateTime, 
 from models.Models import base
 from sqlalchemy.orm import relationship
 # -------------------------------------------------------------------#
-
 # Tabela de funcionários
 # Employers table
 class Employers (base):
@@ -11,14 +10,19 @@ class Employers (base):
     ID = Column("ID", Integer, primary_key=True, autoincrement=True)
     name = Column("name", String)
     password = Column("password", String)
-    sector = Column("sector", String)
-
+    sector_ID = Column(Integer, ForeignKey("Sectors.ID"))
+# -------------------------------------------------------------------#
+#Relacionamento
+#Relationship
+    
     employer_sector = relationship("Sectors", back_populates="employers")
-
-    def __init__(self, name, password, sector):
-        self.name = name
-        self.password = password
-        self.sector = sector
-
     machiningproduction =  relationship("MachiningProduction", back_populates="machiningemployer")
     assemblyproduction = relationship("AssemblyProduction", back_populates="assemblyemployer")
+# -------------------------------------------------------------------#
+    
+    def __init__(self, name, password, sector_ID):
+        self.name = name
+        self.password = password
+        self.sector_ID = sector_ID
+
+   
