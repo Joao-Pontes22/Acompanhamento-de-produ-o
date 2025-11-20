5018826b8bcd1d1154945db03d523dfe0e1ebec2
João Pontes <136093514+Joao-Pontes22@users.noreply.github.com>
2025-11-20 15:42:32 -0300

    Update Empolyers.py

--- models/Empolyers.py
+++ models/Empolyers.py
@@ -10,14 +10,14 @@ class Employers (base):
     ID = Column("ID", Integer, primary_key=True, autoincrement=True)
     name = Column("name", String)
     password = Column("password", String)
-    sector_ID = Column(Integer, ForeignKey("Sectors.ID"))
+    sector_ID = Column("sector_ID",Integer, ForeignKey("Sectors.ID"))
 # -------------------------------------------------------------------#
 #Relacionamento
 #Relationship
     
     employer_sector = relationship("Sectors", back_populates="employers")
-    machiningproduction =  relationship("MachiningProduction", back_populates="machiningemployer")
-    assemblyproduction = relationship("AssemblyProduction", back_populates="assemblyemployer")
+    machining_production =  relationship("Machining_Production", back_populates="machining_employer")
+    assembly_production = relationship("Assembly_Production", back_populates="assembly_employer")
 # -------------------------------------------------------------------#
     
     def __init__(self, name, password, sector_ID):
